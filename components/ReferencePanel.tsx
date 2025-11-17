0c1135244ea97157e29572bb23f5fc27cea02bc4
Marc Schneidewent <marcschneidewent@Marcs-MacBook-Pro.local>
2025-11-17 14:02:15 -0600

    Description of what you changed

--- components/ReferencePanel.tsx
+++ components/ReferencePanel.tsx
@@ -497,6 +497,11 @@ const ReferencePanel: React.FC<ReferencePanelProps> = ({ summary, insights, prog
             </button>
           </div>
         </div>
+
+        {/* Test Card */}
+        <div className="bg-gray-800/50 p-4 rounded-lg border border-gray-700">
+          <h3 className="text-lg font-semibold text-gray-200">Test</h3>
+        </div>
       </div>
     </div>
   );
